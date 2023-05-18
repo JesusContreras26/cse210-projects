ac1add100925d6bfce6aab3ea18f8e0e453563cb
JesusContreras26 <jgcc26@gmail.com>
2023-05-18 11:25:52 -0400

    Learning 3 added

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(7,6);
+
+        fraction3.SetTop(25);
+        fraction3.SetBottom(25);
+
+        Console.WriteLine($"{fraction3.GetTop()}");
+        Console.WriteLine($"{fraction3.GetBottom()}");
+
+        Console.WriteLine($"{fraction3.GetFractionString()}");
+        Console.WriteLine($"{fraction3.GetDecimalValue()}");
     }
 }
